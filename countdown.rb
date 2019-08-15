2d21793caab5125eb3ed99eb76d2ffef3316eacc
imperative-genre-4923 <dreama.lin@yale.edu>
2019-08-15 02:50:17 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,13 @@
 #write your code here
 
-def countdown
+def countdown (integer)
+  until interger == 2 do
+    puts "#{integer - 1} SECONDS!"
+    integer -= 1
+  end
+  if integer >== 1
+    puts "1 SECOND!"
+    puts "0 SECOND"!
+  end
+  "HAPPY NEW YEAR!"
 end
